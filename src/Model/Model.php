748f291f06578d5e7ebbca8cfa47658434426e91
neoan <stefan.roehrl@gmail.com>
2023-01-17 13:31:57 -0500

    - Add methods to attributeHelper (for future consistency) - Add notifier to deletion - Test UUID transformer

--- src/Model/Model.php
+++ src/Model/Model.php
@@ -143,6 +143,7 @@ class Model
             throw new Exception('Store error: not hydratable');
         }
         self::$notify->inform();
+        $this->afterStore();
         return $this;
     }
 
@@ -226,7 +227,7 @@ class Model
     }
 
 
-    public function delete($hard = false)
+    public function delete($hard = false):void
     {
         $primaryKey = self::$interpreter->getPrimaryKey();
 
@@ -240,11 +241,17 @@ class Model
                 $primaryKey => $this->{$primaryKey}
             ]);
         }
+        self::$notify->inform();
     }
 
     public static function paginate(int $page = 1, int $pageSize = 30): Paginate
     {
         return new Paginate($page, $pageSize, static::class);
+    }
+
+    protected function afterStore(): void
+    {
+        self::$notify->inform();
     }
 
 }
--- tests/Mocks/MockModel.php
+++ tests/Mocks/MockModel.php
@@ -4,6 +4,7 @@ namespace Test\Mocks;
 
 use Neoan\Database\Database;
 use Neoan\Model\Attributes\HasMany;
+use Neoan\Model\Attributes\Ignore;
 use Neoan\Model\Attributes\IsPrimaryKey;
 use Neoan\Model\Attributes\IsUnique;
 use Neoan\Model\Attributes\Transform;
@@ -11,6 +12,7 @@ use Neoan\Model\Attributes\Type;
 use Neoan\Model\Collection;
 use Neoan\Model\Model;
 use Neoan\Model\Transformers\Hash;
+use Neoan\Model\Transformers\UUID;
 
 class MockModel extends Model
 {
@@ -25,8 +27,24 @@ class MockModel extends Model
     #[Type('TEXT')]
     public string $email;
 
+    #[Transform(UUID::class)]
+    public string $uuid;
+
     #[HasMany(MockAttachedModel::class,['mockId'=>'id'])]
     public Collection $attached;
+
+    #[Ignore]
+    private bool $hasBeenStored = false;
+
+    function called(): bool
+    {
+        return $this->hasBeenStored;
+    }
+
+    public function afterStore(): void
+    {
+        $this->hasBeenStored = true;
+    }
 
     function ensure(): self
     {
@@ -34,6 +52,7 @@ class MockModel extends Model
         CREATE TABLE IF NOT EXISTS mock(
                     id INTEGER PRIMARY KEY, 
                     email TEXT,
+                    uuid UNIQUE,
                     userName TEXT UNIQUE ,
                     password TEXT)
         ');
